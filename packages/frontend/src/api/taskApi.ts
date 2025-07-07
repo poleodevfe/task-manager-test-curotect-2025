bde5721b917e67cbbe2b297d85038670b2c5093b
poleodevfe <poleodevfe@gmail.com>
2025-07-06 21:54:13 -0400

    feat(fe): build createTask fetch function

--- packages/frontend/src/api/taskApi.ts
+++ packages/frontend/src/api/taskApi.ts
@@ -7,3 +7,31 @@ export interface Task {
 }
 
 const API_URL = '/api/tasks';
+
+//get all tasks
+export const getTask = async (): Promise<Task[]> => {
+  const response = await fetch(API_URL);
+  if (!response.ok) {
+    throw new Error('Failed to fetch tasks');
+  }
+  return await response.json();
+};
+
+// create a new task
+export const createTask = async (
+  newTask: Omit<Task, 'id' | 'completed'>
+): Promise<Task> => {
+  const response = await fetch(API_URL, {
+    method: 'POst',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newTask),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to create task');
+  }
+
+  return await response.json();
+};
