cee37f6a0995e55caf6b5cf267a476fac8f0330f
poleodevfe <poleodevfe@gmail.com>
2025-07-06 20:51:08 -0400

    fix(db): error where prisma not getting initialized in docker backend build

--- packages/backend/src/lib/prisma.ts
+++ packages/backend/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma';
 
 // Se crea una instancia única del cliente de Prisma
 const prisma = new PrismaClient();
