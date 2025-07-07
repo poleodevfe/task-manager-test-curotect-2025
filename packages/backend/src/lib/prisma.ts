53dfa1a53e4291be006a1e0590ce6593cf7a2d10
poleodevfe <poleodevfe@gmail.com>
2025-07-07 11:53:12 -0400

    fix(ci): wrong route to prisma

--- packages/backend/src/lib/prisma.ts
+++ packages/backend/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from '../generated/prisma';
 
 // Se crea una instancia única del cliente de Prisma
 const prisma = new PrismaClient();
